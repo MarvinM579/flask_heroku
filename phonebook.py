e172816b64f290931446e4bd157ca394290b3736
Marvin Martinez <marvinmartinez579@gmail.com>
2017-01-05 11:36:06 -0600

    phonebook test

--- phonebook.py
+++ phonebook.py
@@ -28,8 +28,6 @@ DBNAME = os.environ.get('DBNAME', True)
 # Routing for your application.
 ###
 
-app = Flask('phone book')
-
 
 @app.route('/')
 def index():
@@ -131,35 +129,34 @@ def submit_update():
     else:
         raise Exception("ERROR")
     return redirect('/contacts')
-# app.secret_key = 'cn0hn42'
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
 
-# @app.route('/<file_name>.txt')
-# def send_text_file(file_name):
-#     """Send your static text file."""
-#     file_dot_text = file_name + '.txt'
-#     return app.send_static_file(file_dot_text)
-#
-#
-# @app.after_request
-# def add_header(response):
-#     """
-#     Add headers to both force latest IE rendering engine or Chrome Frame,
-#     and also to cache the rendered page for 10 minutes.
-#     """
-#     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-#     response.headers['Cache-Control'] = 'public, max-age=600'
-#     return response
-#
-#
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     """Custom 404 page."""
-#     return render_template('404.html'), 404
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
