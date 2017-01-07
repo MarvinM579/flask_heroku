495617578ac64635438942d7b197c7cb12053552
Marvin Martinez <marvinmartinez579@gmail.com>
2017-01-07 16:17:42 -0600

    na

--- app.py
+++ app.py
@@ -52,8 +52,6 @@ def submit_login():
         if user.password == password:
             session['name'] = user.name
             return redirect('/')
-            if session['name'] == 'Jessica'
-                return redirect('/jessica')
         else:
             return redirect('/login')
     else:
